df40331226932d65dc9cd9e36e33115a73fb13cb
Kauã Gabriell <kauagabriell64@gmail.com>
2026-04-07 16:47:11 -0300

    feat: updaate endpoints for testing

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express';
 
 class ProductsController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    return response.status(200).json({ message: 'Ok' });
+  }
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    return response.status(201).json({ message: 'Ok' });
+  }
 }
 
-export { ProductsController }
+export { ProductsController };
